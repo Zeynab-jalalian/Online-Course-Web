7217a868a14dbe89967f5f18c82e4a76705aa687
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-06-03 12:43:34 +0330

    responsive + js

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-const header=document.querySelector("header");
+const header = document.querySelector("header");
 
-window.addEventListener("scroll",()=>{
-    header.classList.toggle("sticky",window.scrollY > 0)
+window.addEventListener("scroll", () => {
+    header.classList.toggle("sticky", window.scrollY > 0);
 })
+
+let menu = document.getElementById("menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menu.addEventListener("click", () => {
+    navbar.classList.toggle("open");
+    menu.classList.toggle("bx-x");
+})
+
+window.onscroll = () => {
+    navbar.classList.remove("open");
+    menu.classList.remove("bx-x");
+}
